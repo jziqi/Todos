12269b0b80413c86cbce970ddaef1661776e95ce
zi qi <jiangzziqi@gmail.com>
2023-07-29 11:11:34 +0800

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by T Krobot on 29/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
